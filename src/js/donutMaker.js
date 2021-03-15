a55724743f8d792f3e3cd0904a11ba946c71f0fc
unknown <cuevastl@gmail.com>
2021-03-14 21:23:25 -0400

    Added a donutClick function and activateAutoClickers function.

--- src/js/donutMaker.js
+++ src/js/donutMaker.js
@@ -1,5 +1,7 @@
-function Donuts() {
-    this._count = 200;
+function DonutMaker() {
+    this._count = 400;
+    this._aCCount = 0;
+    this._aCCost = 100;
 
     this.addDonut = function(){
         this._count +=  1;
@@ -15,18 +17,22 @@ function Donuts() {
         }
     };
 
-};
+    this.donutClick = function(obj){
+        obj._count += obj._aCCount;
+        console.log(obj._count);
+    };
 
-function AutoClickers() {
-    this._count = 0;
-    this._cost = 100;
+    this.activateAutoClickers = function(obj){
+        setInterval(this.donutClick, 1000, obj);
+    }
+
     
-    this.addAutoClicker = function(donuts){
-        if(donuts._count >= this._cost){
-            donuts._count -= this._cost;
-            this._count += 1;
-            this._cost *= 1.1;
-            this._cost = Math.round(this._cost);
+    this.addAutoClicker = function(){
+        if(this._count >= this._aCCost){
+            this._count -= this._aCCost;
+            this._aCCount += 1;
+            this._aCCost *= 1.1;
+            this._aCCost = Math.round(this._aCCost);
         }
         else{
         console.log('You do not have enough donuts to purchase this PowerUp');
@@ -34,24 +40,19 @@ function AutoClickers() {
     };
 
     this.getAutoClickerCount = function(){
-        if(this._count < 0){
-            this._count = 0;
-            return this._count;
+        if(this._aCCount < 0){
+            this._aCCount = 0;
+            return this._aCCount;
         }
         else{
-        return this._count;
+        return this._aCCount;
         }
     };
-
-    // this.getAutoClickerCost = function(){
-    //     if(this._cost < 0){
-    //         this._cost = 0;
-    //         return this._cost;
-    //     };
-    //     return this._cost;
-    // }
 };
 
-const myDonuts = new Donuts();
-const myAutoClickers = new AutoClickers();
-myDonuts.addDonut();
+const myDonutMaker = new DonutMaker();
+myDonutMaker.addDonut();
+myDonutMaker.addAutoClicker();
+myDonutMaker.addAutoClicker();
+myDonutMaker.activateAutoClickers(myDonutMaker);
+
