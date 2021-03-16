e559f6cc94c89446e4901dedd4ade5cec641d2cd
unknown <cuevastl@gmail.com>
2021-03-16 13:47:41 -0400

    Added nav dropdown and link to Fred the Baker.

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+function navDropdown() {
+    document.getElementById("navDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
